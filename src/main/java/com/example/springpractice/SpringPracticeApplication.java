83d87bf5517374f97e07889f2b996beba178056e
JangHwanPark <wkd5878@gmail.com>
2024-03-30 19:27:52 +0900

    엔드포인트 생성 ('/', 'name')

--- src/main/java/com/example/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/example/springpractice/SpringPracticeApplication.java
@@ -2,12 +2,22 @@ package com.example.springpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringPracticeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringPracticeApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String home(@RequestParam(value = "name", defaultValue = "기본값?") String name) {
+        return String.format("이름!! %s!", name);
+    }
+    @GetMapping("name")
+    public String name(@RequestParam(value = "name", defaultValue = "이름!!") String name) {
+        return String.format("Name %s!", name);
+    }
 }
