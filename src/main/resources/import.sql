c354c2c770a893a61cd2fe63f8e734931de93de0
user <wkd5878@gmail.com>
2024-06-23 21:01:18 +0900

    feat: 스프링 부트 실행시 테스트디비내 데이터 추가

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (user_id, name, password, email, role)
+VALUES (1, 'testuser', 'password', 'test@example.com', 'USER');
+INSERT INTO user (user_id, name, password, email, role)
+VALUES (2, 'adminuser', 'adminpassword', 'admin@example.com', 'ADMIN');
